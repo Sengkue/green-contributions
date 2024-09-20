f6a5a3c4f243b3017a5cc4bf37b7537b973c7f13
sengvang.blo <skvgithub@gmail.com>
2024-09-20 11:22:48 +0700

    commit 20/09/2024 11:22:48.35

--- auto_commit.bat
+++ auto_commit.bat
@@ -2,7 +2,7 @@
 :start
 set CURRENT_TIME=%DATE% %TIME%
 git add .
-git commit -m "Auto commit from GitHub Actions at %CURRENT_TIME%"
+git commit -m "commit %CURRENT_TIME%"
 git push
 timeout /t 20
 goto start
