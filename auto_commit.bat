8275bd19430cf33f39b4426e7e1a24dab0eaba9a
sengvang.blo <skvgithub@gmail.com>
2024-09-20 11:14:02 +0700

    Automated commit

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,16 +1,7 @@
-@REM @echo off
-@REM :start
-@REM git add .
-@REM git commit -m "Automated commit"
-@REM git push
-@REM timeout /t 60
-@REM goto start
-
-
 @echo off
 :start
 git add .
 git commit -m "Automated commit"
 git push
-ping -n 1 -w 10 127.0.0.1 > nul
+timeout /t 60
 goto start
