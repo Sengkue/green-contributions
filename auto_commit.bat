b3846709200831222c266a0579e52b89b02366b7
sengvang.blo <skvgithub@gmail.com>
2024-09-20 11:16:42 +0700

    Auto commit from GitHub Actions at 20/09/2024 11:16:42.47

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,7 +1,8 @@
 @echo off
 :start
+set CURRENT_TIME=%DATE% %TIME%
 git add .
-git commit -m "Automated commit"
+git commit -m "Auto commit from GitHub Actions at %CURRENT_TIME%"
 git push
-timeout /t 60
+timeout /t 20
 goto start
